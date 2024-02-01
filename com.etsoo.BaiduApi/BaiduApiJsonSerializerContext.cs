6ea7d2ec1bff4eefe4f0a21cdabbded451826774
Garry Xiao <xz@etsoo.com>
2024-02-01 22:08:03 +1300

    Upgrade dependencies

--- com.etsoo.BaiduApi/BaiduApiJsonSerializerContext.cs
+++ com.etsoo.BaiduApi/BaiduApiJsonSerializerContext.cs
@@ -6,7 +6,9 @@ namespace com.etsoo.BaiduApi
     /// Baidu API JSON serializer context
     /// 百度API JSON序列化上下文
     /// </summary>
+    [JsonSourceGenerationOptions(PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase, DictionaryKeyPolicy = JsonKnownNamingPolicy.CamelCase, PropertyNameCaseInsensitive = true)]
     [JsonSerializable(typeof(Maps.Place.RQ.MapBaseRQ))]
+    [JsonSerializable(typeof(Maps.Place.BaseResponse))]
     public partial class BaiduApiJsonSerializerContext : JsonSerializerContext
     {
     }
@@ -15,9 +17,9 @@ namespace com.etsoo.BaiduApi
     /// Baidu API call JSON serializer context
     /// 百度API调用JSON序列化上下文
     /// </summary>
-    [JsonSourceGenerationOptions(PropertyNamingPolicy = JsonKnownNamingPolicy.SnakeCaseLower)]
+    [JsonSourceGenerationOptions(PropertyNamingPolicy = JsonKnownNamingPolicy.SnakeCaseLower, DictionaryKeyPolicy = JsonKnownNamingPolicy.SnakeCaseLower, PropertyNameCaseInsensitive = true)]
     [JsonSerializable(typeof(Maps.Place.BaseResponse))]
-    public partial class BaiduApiCallJsonSerializerContext : JsonSerializerContext
+    internal partial class BaiduApiCallJsonSerializerContext : JsonSerializerContext
     {
     }
 }
